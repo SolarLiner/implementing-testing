02f83c89c0b7b2ab1afb3d4f4f4698e68d0c629d
Nathan Graule <solarliner@gmail.com>
2017-08-06 15:35:59 +0700

    Added some tests

--- src/secure_class.py
+++ src/secure_class.py
@@ -2,11 +2,13 @@
 
 class VeryImportant(object):
     """This class is very secure and must be tested thouroughly."""
-    def __init__(name="John Doe"):
-        self.name = name
+    def __init__(self, user_name):
+        if not user_name:
+            user_name = "John Doe"
+        self.name = user_name
 
     def __str__(self):
-        return self.name
+        return "<VeryImportant: {}>".format(self.name)
 
     def say_hello():
         """Static function saying hello."""
---
+++ tests/test.py
@@ -0,0 +1,24 @@
+import unittesting
+from src.secure_class import *
+
+class VeryImportantTestCase:
+     very_important = None
+    def setup(self):
+        print "VeryImportant module test"
+        self.very_important = VeryImportant("Nathan")
+    
+    def teardown(self):
+        pass # nothing to do here
+
+    def test_hello_static(self):
+        print "Static hello()"
+        assert VeryImportant.say_hello() == "Hello!"
+    
+    def test_hello_instance(self):
+        print "Instance hello()"
+        assert self.very_important.say_hello() == "Hello Nathan!"
+        assert self.very_important.say_hello() != "Hello John Doe!" # Default case in constructor
+
+    def test_class_str(self):
+        print "Class __str__()"
+        assert vi.__str__() == "<VeryImportant: Nathan>"
